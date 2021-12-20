6c8b02950e39f8be122c72a3a0cf597d8e9456f8
Ronaldo Nolasco <ronaldo.nolasco@gestionysistemas.com>
2021-12-20 03:48:34 -0500

    starting project

---
+++ src/main/java/pe/com/arboleda/ArboledaApplication.java
@@ -0,0 +1,13 @@
+package pe.com.arboleda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArboledaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArboledaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/arboleda/ArboledaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.arboleda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArboledaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
